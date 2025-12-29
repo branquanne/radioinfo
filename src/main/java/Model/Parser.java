96fbe15199434222f26173c47ad20951b913629a
Bran Quanne <branmq@gmail.com>
2025-12-29 22:49:54 +0100

    added methods for parsing channels and programs

--- src/main/java/Model/Parser.java
+++ src/main/java/Model/Parser.java
@@ -1,12 +1,51 @@
 package Model;
 
+import Model.domain.Channel;
+import Model.domain.Program;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.net.http.HttpResponse;
+import java.util.Collections;
+import java.util.List;
 
 public class Parser {
-  String json;
+    private String json;
+    private ObjectMapper mapper = new ObjectMapper();
 
-  public void parseJSON(HttpResponse response) {
-    json = response.toString();
-  }
 
+    public Parser(HttpResponse<String> response) {
+        this.json = response.body();
+    }
+
+    public List<Channel> parseChannels() {
+        try {
+            JsonNode root = mapper.readTree(json);
+            JsonNode channelIsNode = root.path("channels");
+            if (channelIsNode.isMissingNode() || !channelIsNode.isArray()) {
+                return Collections.emptyList();
+            }
+            return mapper.convertValue(channelIsNode, new TypeReference<List<Channel>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Program> parsePrograms() {
+        try {
+            JsonNode root = mapper.readTree(json);
+            JsonNode programsNode = root.path("programs");
+            if (programsNode.isMissingNode() || !programsNode.isArray()) {
+                return Collections.emptyList();
+            }
+            return mapper.convertValue(programsNode, new TypeReference<List<Program>>() {
+            });
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
