c3af9b3273d776fea97da84234a4d9ed841dfcc2
Bran Quanne <branmq@gmail.com>
2025-12-29 22:48:57 +0100

    Completed methods for fetching channels and programs

--- src/main/java/Model/ApiClient.java
+++ src/main/java/Model/ApiClient.java
@@ -12,46 +12,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ApiClient {
-  List<Channel> channels = new ArrayList<>();
+    List<Channel> channels = new ArrayList<>();
 
-  public ApiClient() {
-  }
-
-  public void returnInfo() {
-  }
-
-  private HttpResponse fetchChannels() {
-    try {
-      URI uri = URI.create("https://api.sr.se/api/v2/channels?format=json&indent=true");
-      HttpClient client = HttpClient.newHttpClient();
-      HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      return response;
-
-    } catch (InterruptedException | IOException e) {
-      throw new RuntimeException(e);
+    public ApiClient() {
     }
 
-  }
-
-  private HttpResponse fetchPrograms() {
-    try {
-      URI uri = URI.create("https://api.sr.se/api/v2/scheduledepisodes?format=json&indent=true");
-      HttpClient client = HttpClient.newHttpClient();
-      HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
-      HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-      return response;
-
-    } catch (InterruptedException | IOException e) {
-      throw new RuntimeException(e);
+    public void returnInfo() {
     }
-  }
 
-  public List<Channel> getChannels() {
-    return channels;
-  }
+    public List<Channel> fetchChannels() {
+        try {
+            URI uri = URI.create("https://api.sr.se/api/v2/channels?format=json&indent=true");
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            Parser p = new Parser(response);
+            return p.parseChannels();
 
-  public List<Program> getPrograms(Channel channel) {
-    return channel.getPrograms();
-  }
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public List<Program> fetchPrograms() {
+        try {
+            URI uri = URI.create("https://api.sr.se/api/v2/scheduledepisodes?format=json&indent=true");
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            Parser p = new Parser(response);
+            return p.parsePrograms();
+
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Channel> getChannels() {
+        return channels;
+    }
+
+    public List<Program> getPrograms(Channel channel) {
+        return channel.getPrograms();
+    }
 }
