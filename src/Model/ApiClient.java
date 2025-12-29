e37f6b99e698c85791d61629c4cb9d2a37526ed1
Bran Quanne <branmq@gmail.com>
2025-12-29 13:06:42 +0100

    Got the request from api to work and im getting indented JSON for both channels and programs

--- src/Model/ApiClient.java
+++ src/Model/ApiClient.java
@@ -5,8 +5,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 public class ApiClient {
+    List<String> channels;
 
     Parser parser = new Parser();
 
@@ -17,9 +19,9 @@ public class ApiClient {
     }
 
 
-    public void getChannels() {
+    private void fetchChannels() {
         try {
-            URI uri = URI.create("http://api.sr.se/api/v2/channels");
+            URI uri = URI.create("https://api.sr.se/api/v2/channels?format=json&indent=true");
             HttpClient client = HttpClient.newHttpClient();
             HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -28,6 +30,18 @@ public class ApiClient {
         } catch (InterruptedException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
+    private void fetchPrograms() {
+        try {
+            URI uri = URI.create("https://api.sr.se/api/v2/scheduledepisodes?format=json&indent=true");
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
