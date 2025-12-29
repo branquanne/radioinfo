fe37a6124f2ef447ff9d2b2336eaea0fb6663b07
Bran Quanne <branmq@gmail.com>
2025-12-29 14:16:41 +0100

    setup domain for channel and program

--- src/main/java/Model/domain/Channel.java
+++ src/main/java/Model/domain/Channel.java
@@ -1,4 +1,27 @@
 package Model.domain;
 
+import java.awt.*;
+import java.util.List;
+
 public class Channel {
+    private List<Program> programs;
+    private int channelId;
+    private String channelName;
+    private Image thumbnail;
+
+    public List<Program> getPrograms() {
+        return programs;
+    }
+
+    public int getChannelId() {
+        return channelId;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public Image getThumbnail() {
+        return thumbnail;
+    }
 }
--- src/main/java/Model/domain/Program.java
+++ src/main/java/Model/domain/Program.java
@@ -1,4 +1,37 @@
 package Model.domain;
 
+import java.awt.*;
+import java.time.LocalDateTime;
+
 public class Program {
+    private int programId;
+    private String programTitle;
+    private String description;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private Image thumbnail;
+
+    public int getProgramId() {
+        return programId;
+    }
+
+    public String getProgramTitle() {
+        return programTitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public Image getThumbnail() {
+        return thumbnail;
+    }
 }
