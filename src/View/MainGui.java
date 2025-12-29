501c8a68bf9118422241dc8d3b4ca8a6ff4adc7b
Bran Quanne <branmq@gmail.com>
2025-12-29 12:00:49 +0100

    Started initializing JMenuBar and its components

--- src/View/MainGui.java
+++ src/View/MainGui.java
@@ -3,21 +3,40 @@ package View;
 import javax.swing.*;
 
 public class MainGui {
-  TableGui tg = new TableGui();
+    private static JFrame frame;
+    private static JMenuBar menuBar;
+    private static JMenu channelsMenu;
+    private static JMenu programsMenu;
+    private static JMenuItem showChannelsTable;
+    private static JMenuItem showProgramsTable;
 
-  public MainGui() {
 
-  }
+    public MainGui() {
 
-  public static void main(String[] args) {
-    SwingUtilities.invokeLater(MainGui::showGUI);
-  }
+    }
 
-  private static void showGUI() {
-    JFrame frame = new JFrame("Radio Info");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(600, 600);
-    frame.setVisible(true);
-  }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(MainGui::showGUI);
+    }
+
+    private static void showGUI() {
+        JFrame frame = new JFrame("Radio Info");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 600);
+
+
+        frame.setVisible(true);
+    }
+
+    private static void initMenu() {
+        menuBar = new JMenuBar();
+
+        channelsMenu = new JMenu();
+        programsMenu = new JMenu();
+
+        showChannelsTable = new JMenuItem("Show Table");
+        showProgramsTable = new JMenuItem("Show Table");
+
+    }
 
 }
