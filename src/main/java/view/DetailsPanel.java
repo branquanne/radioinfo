477ae55cf58be961e05b6704b8402dcf924513bd
Bran Quanne <branmq@gmail.com>
2026-01-15 11:39:15 +0100

    copied from Gui.java

--- src/main/java/view/DetailsPanel.java
+++ src/main/java/view/DetailsPanel.java
@@ -1,4 +1,23 @@
 package view;
 
-public class DetailsPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class DetailsPanel extends JPanel {
+    private final JLabel imageLabel = new JLabel();
+    private final JTextArea descriptionsArea = new JTextArea();
+
+    public DetailsPanel() {
+        super(new BorderLayout(8, 8));
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        descriptionsArea.setLineWrap(true);
+        descriptionsArea.setEditable(false);
+
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.add(imageLabel, BorderLayout.CENTER);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(new JScrollPane(descriptionsArea), BorderLayout.CENTER);
+    }
 }
