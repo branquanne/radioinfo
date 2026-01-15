97c9fdfb70c56499b58e8ead72f6fceb5aae9720
Bran Quanne <branmq@gmail.com>
2026-01-15 12:14:28 +0100

    copied logic over from Gui.juava

--- src/main/java/view/ProgramsPanel.java
+++ src/main/java/view/ProgramsPanel.java
@@ -1,4 +1,23 @@
 package view;
 
-public class ProgramsPanel {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class ProgramsPanel extends JPanel {
+    private final JTable programsTable = new JTable();
+
+    public ProgramsPanel() {
+        super(new BorderLayout());
+        add(new JScrollPane(programsTable), BorderLayout.CENTER);
+    }
+
+    public void setModel(DefaultTableModel model) {
+        SwingUtilities.invokeLater(() -> programsTable.setModel(model));
+    }
+
+    public JTable getTable() {
+        return programsTable;
+    }
+
 }
