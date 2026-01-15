8cabd4189efeed5e36ac6d773204f8651311c292
Bran Quanne <branmq@gmail.com>
2026-01-15 12:40:59 +0100

    Changed MenuBar to AppMenuBar since it collided with java.awt.MenuBar

--- src/main/java/view/MainFrame.java
+++ src/main/java/view/MainFrame.java
@@ -1,32 +1,28 @@
 package view;
 
-import model.Channel;
-
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
 
-public class Gui {
-    private JFrame frame;
-    private JMenuBar menuBar;
-    private JMenu channelsMenu;
-    private JTable programsTable;
-    private JTable channelsTable;
-    private JPanel detailsPanel;
-    private JLabel detailsLabel;
-    private JTextArea detailsDescriptionArea;
+public class MainFrame {
+    private final JFrame frame = new JFrame("Radio Info");
+    private final ChannelsPanel channelsPanel = new ChannelsPanel();
+    private final ProgramsPanel programsPanel = new ProgramsPanel();
+    private final DetailsPanel detailsPanel = new DetailsPanel();
+    private final AppMenuBar appMenuBar;
+
+    public MainFrame(){
+
+    }
+
+
 
     public void show() {
         frame = new JFrame("Radio Info");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 800);
         initMenu();
-        frame.setJMenuBar(menuBar);
+        frame.setJMenuBar(appMenuBar);
 
         channelsTable = new JTable(new DefaultTableModel(new String[]{"Channel", "Description"}, 0));
         programsTable = new JTable(new DefaultTableModel(new String[]{"Program", "Start time", "End time"}, 0));
@@ -38,15 +34,15 @@ public class Gui {
     }
 
     private void initMenu() {
-        menuBar = new JMenuBar();
+        appMenuBar = new JMenuBar();
 
         channelsMenu = new JMenu("Channels");
         JMenu viewMenu = new JMenu("View");
         JMenuItem showChannels = new JMenuItem("Show Channels");
         showChannels.addActionListener(e -> showChannelsTable());
         viewMenu.add(showChannels);
-        menuBar.add(channelsMenu);
-        menuBar.add(viewMenu);
+        appMenuBar.add(channelsMenu);
+        appMenuBar.add(viewMenu);
     }
 
 
@@ -76,8 +72,8 @@ public class Gui {
 
 
 
-        menuBar.revalidate();
-        menuBar.repaint();
-    }
+        appMenuBar.revalidate();
+        appMenuBar.repaint();
+
 
 }
