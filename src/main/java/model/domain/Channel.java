dbc644ac5b49263bb92e4e29abcd67cec061bd6b
Bran Quanne <branmq@gmail.com>
2026-01-15 09:42:37 +0100

    updated to use image instead of imagetemplate

--- src/main/java/model/domain/Channel.java
+++ src/main/java/model/domain/Channel.java
@@ -7,38 +7,38 @@ import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Channel {
-  private List<Program> programs;
-  private int channelId;
-  private String channelName;
-  private String tagline;
+    private List<Program> programs;
+    private int channelId;
+    private String channelName;
+    private String tagline;
 
-  @JsonProperty("image")
-  private String thumbnailLink;
+    @JsonProperty("image")
+    private String thumbnailLink;
 
-  public List<Program> getPrograms() {
-    return programs;
-  }
+    public List<Program> getPrograms() {
+        return programs;
+    }
 
-  @JsonProperty("id")
-  public int getChannelId() {
-    return channelId;
-  }
+    @JsonProperty("id")
+    public int getChannelId() {
+        return channelId;
+    }
 
-  @JsonProperty("name")
-  public String getChannelName() {
-    return channelName;
-  }
+    @JsonProperty("name")
+    public String getChannelName() {
+        return channelName;
+    }
 
-  @JsonProperty("tagline")
-  public String getTagline() {
-    return tagline;
-  }
+    @JsonProperty("tagline")
+    public String getTagline() {
+        return tagline;
+    }
 
-  public String getThumbnailLink() {
-    return thumbnailLink;
-  }
+    public String getThumbnailLink() {
+        return thumbnailLink;
+    }
 
-  public void setPrograms(List<Program> programs) {
-    this.programs = programs;
-  }
+    public void setPrograms(List<Program> programs) {
+        this.programs = programs;
+    }
 }
